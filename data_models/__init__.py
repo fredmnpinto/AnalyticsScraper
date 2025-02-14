5c8d95c6ec5f0bfc8f0708b8c7b90901fd908b38
Frederico N. Pinto <fredmnpinto@gmail.com>
2025-02-14 09:34:24 +0000

    Removed DB_URL env var

--- data_models/__init__.py
+++ data_models/__init__.py
@@ -1,6 +1,5 @@
 import csv
 import os
-from dotenv import load_dotenv
 
 from tqdm import tqdm
 from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, CHAR, Float, Index
@@ -13,8 +12,6 @@ Base = declarative_base()
 import data_models.AirQualitySummary
 import data_models.GovermentBudgetSurplus
 
-load_dotenv()
-
 # Returns database session
 def setup_database():
     # Define the connection string
@@ -25,7 +22,7 @@ def setup_database():
     database = os.getenv('DB_DATABASE')
 
     # Format the connection string
-    connection_string = os.getenv('DB_URL') or f'postgresql://{username}:{password}@{host}:{port}/{database}'
+    connection_string = f'postgresql://{username}:{password}@{host}:{port}/{database}'
     engine = create_engine(connection_string)
 
     Base.metadata.create_all(engine)
